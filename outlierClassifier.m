ad77f7ac0223d8bd98a9fa39a371ba7c75defb77
LiandaDuan <dlp@LiandaDuandeMacBook-Pro.local>
2017-04-23 17:40:05 +0800

    first commitment

---
+++ outlierClassifier.m
@@ -0,0 +1,15 @@
+function re=outlierClassifier(X)
+%syms mu Outlier
+si=size(X,1);
+data=X;
+mu = mean(data);
+OutLier=std(data)*3
+for i=1:1:si
+    if abs(data(i)-mu)>OutLier
+        errorLabelMatrix(i)=1;
+    else
+        errorLabelMatrix(i)=0;
+    end
+end
+
+re=errorLabelMatrix
